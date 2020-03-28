4dfe22b3e6b8313f3d3e1cd69f88dfdfd3cf5150
dev-augustin <dev.augustin00@gmail.com>
2020-03-28 18:58:08 -0500

    added color and state

--- boxcolor/src/App.js
+++ boxcolor/src/App.js
@@ -4,14 +4,21 @@ import './App.css';
 class App extends React.Component() {
   constructor(){
     super()
+    this.state={
+      color:'green'
+    }
   }
   render()
   {
     return (
     <div> 
-      <div className="App" >
-      Count= 
-      </div>
+      <div className="App" 
+        style=
+        {{backgroundColor: this.state.color}}>
+      
+      <p>Count= </p>
+      </div>  
+     
       {/* <h3>Count= </h3> */}
     </div>
    
@@ -20,3 +27,46 @@ class App extends React.Component() {
 }
 
 export default App;
+
+//export default class App extends Component{//
+// class App extends Component{
+//   constructor(props){
+//     super(props);
+//     this.state={
+//       color: "green",
+//       //clickCounter:0
+//     }
+//     this.onChange=this.onChange.bind(this);
+//   }  
+
+// onChange = (e) => {
+//   this.setState({
+//      color:'orange',
+//      //clickCounter: this.setState.clickCounter++
+//   })
+//   console.log("click counter==>", this.state.clickCounter)
+// } 
+
+//   render(){
+//     return(
+//       <div className="App">
+//         <h1> Box Color Change</h1>
+//         <div className= 'colorBox' 
+//         style={{backgroundColor:this.state.color}}
+//         onClick={this.onChange}
+//         >
+//           <p>
+//             Click here
+//           </p>
+//         </div>
+//       </div>
+//     );
+//   }
+// }  
+
+
+
+
+
+// export default App;
+
