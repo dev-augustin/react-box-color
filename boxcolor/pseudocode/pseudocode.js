438ef581c97e09a1ac359a241d769e7174cc0dce
dev-augustin <dev.augustin00@gmail.com>
2020-03-28 20:05:19 -0500

    added pseudocode

---
+++ boxcolor/pseudocode/pseudocode.js
@@ -0,0 +1,7 @@
+// CREATE <div> box in Render method
+// ADD color to box using state in constructor method
+// WHEN box is clicked color should change
+// To achieve that create onClick handle event and assign it to colorChange function
+// In colorChange method, use setState to change the color
+// for clickCounter, initialize it in State and increment it under colorChange
+// box color should toggle back to its original color
