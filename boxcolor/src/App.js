07ccab0aa7a54b2924b1e6abd4948839a3ae03da
dev-augustin <dev.augustin00@gmail.com>
2020-03-28 19:52:42 -0500

    added heading

--- boxcolor/src/App.js
+++ boxcolor/src/App.js
@@ -7,28 +7,47 @@ class App extends React.Component {
     super(props);
     this.state={
       color:'green',
-      counterClick:0
+      counterClick:0,
+      isColor:true
     }
     //this.onChange=this.onChange.bind(this);
   }
-  onChange(){
-    this.setState(
+  colorChange(){
+    if(this.state.isColor)
+      this.setState(
       {
         color:'yellow', 
         counterClick: this.state.counterClick+1
       }
     )
+    else 
+    this.setState(
+      {
+        color:'orange', 
+        counterClick: this.state.counterClick+1
+      }
+    )
     console.log("click counter: ", this.state.counterClick)
   }
+  // colorChange(){
+  //   this.setState(
+  //     {
+  //       color:'yellow', 
+  //       counterClick: this.state.counterClick+1
+  //     }
+  //   )
+  //   console.log("click counter: ", this.state.counterClick)
+  // }
   render()
   {
     return (
     <div> 
+      <h2>BoxColor Change</h2>
       <div className="App" 
         style=
-        {{backgroundColor: this.state.color}} onClick={()=>this.onChange()}
+        {{backgroundColor: this.state.color}} onClick={()=>this.colorChange()}
         >
-      <p>Count= {this.state.counterClick}</p>
+      <p id='countText'>Count= {this.state.counterClick}</p>
       </div>  
       {/* <h3>Count= </h3> */}
     </div>
